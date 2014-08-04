68347e9992f9009278a6ef7d90d5676f30ca4cc9
Everett Wetchler <evee746@gmail.com>
2014-08-04 00:50:44 -0700

    Add (hopefully) functional Everett-phone-detection code

--- app/views.py
+++ app/views.py
@@ -1,15 +1,43 @@
-from flask import render_template
-from app import app
-
 import datetime
+import hashlib
+import logging
+import os
+import subprocess
+
+from flask import render_template
 import pytz
 
+from app import app
+
+logging.basicConfig(
+    filename='example.log',
+    level=logging.DEBUG,
+    format='%(asctime)s:%(levelname)s:%(message)s')
+
+PHONE_MAC_SUFFIX = '7b:5b:3c'
 TZ = pytz.timezone('US/Pacific')
+
+def check_away():
+    lines = subprocess.check_output(['netstat', '-nr']).splitlines()
+    for line in lines:
+        if line.startswith('192.168.1.') and len(line.split()) > 1:
+            ip, mac = line.split()[:2]
+            if mac.endswith(PHONE_MAC_SUFFIX):
+                try:
+                    x = subprocess.check_output(['ping', '-c1', '-t', '1', ip])
+                    return False
+                except subprocess.CalledProcessError:
+                    return True
+    return True
 
 @app.route('/')
 def index():
-	now = datetime.datetime.now(tz=pytz.utc).astimezone(TZ)
-	return render_template('index.html',
-		location='San Francisco, CA',
-		date=now.strftime('%A, %B %d %Y'),
-		hours=now.strftime('%I:%M %p'))
+    away = check_away()
+    now = datetime.datetime.now(tz=pytz.utc).astimezone(TZ)
+    datestr = now.strftime('%A, %B %d %Y')
+    hourstr = now.strftime('%I:%M %p')
+    logging.info('Everett is %s', 'away' if away else 'home')
+    return render_template('index.html',
+        date=datestr,
+        hours=hourstr,
+        away=away)
